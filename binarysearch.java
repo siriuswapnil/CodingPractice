609f4a1d624709e731d4678e70ff4f10d8060206
swapnil <reach_swapnil@outlook.com>
2020-07-06 09:22:22 +0530

    binsearch

---
+++ binarysearch.java
@@ -0,0 +1,28 @@
+class binarysearch {
+
+       int search(int[] nums, int target) {
+            int low = 0, high = nums.length-1;
+            while(low<=high){
+               int mid = low + (high-low)/2;
+                if(target == nums[mid]){
+                    return mid;
+                }
+                else if(target>nums[mid]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid -1;
+                }
+            }
+            return -1;
+       }
+    
+    public static void main(String[] args) {
+        binarysearch ob = new binarysearch();
+        int arr[] = {1,2,3,4,5};
+        int target = 5;
+        int result = ob.search(arr, target);
+        System.out.println(result);
+    }
+
+}
